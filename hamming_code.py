c7c6c09ebe9ae066f626f5b99916885ba1e29713
Jean David <jeandavid54@aol.com>
2021-08-04 10:55:19 +0200

    Add files via upload

--- hamming_code.py
+++ hamming_code.py
@@ -5,12 +5,23 @@ import numpy as np
 import functools
 from operator import xor
 
+# block n x n, n must be power of 2 ??
+n = 4
+digits = int(np.log2(n**2))
+
 # random blocks can be uncorrect coded blocks
-bloc = np.random.randint(0,2,16)
+bloc = np.random.randint(0,2,n**2)
 locations_w_ones = [i for i, bit in enumerate(bloc) if bit]
-print(bloc.reshape(4,4))
+# binary 
+bin_locations = [bin(i).replace("0b", "").zfill(digits)  for i, bit in enumerate(bloc) if bit]
+print(bin_locations)
+
+print(bloc.reshape(n,n))
 print(locations_w_ones)
 
+
+# xor all the list, as "xor 1" twice gives back initial value,
+# xoring a list gives the parity of numbers of ones in that list
 def xor1(list):
     # List XOR
     # Using reduce() + lambda + "^" operator
@@ -19,10 +30,14 @@ def xor1(list):
 
 def xor2(list):
     # List XOR
-    # Using reduce() + operator.ixor
+    # Using reduce() + operator.xor
     res = functools.reduce(xor, list)
     return res
-  
+
+# here we "xor" a list of case addresses. xor operates in binary
+# if an address is in binary form a d-digit form, for 8x8 grid, we have a 6-digit number as log2(8*8) = 6
+# xoring all the locations with 1 will give the address of the error case
+# error case = 0 means no code error
 print('xor1 : error in ', xor1(locations_w_ones))
 print('xor2 : error in' , xor2(locations_w_ones))
 
@@ -30,7 +45,7 @@ print('xor2 : error in' , xor2(locations_w_ones))
 error_at = xor2(locations_w_ones)
 print('correcting error ....', error_at)
 bloc[error_at] = not bloc[error_at]
-print(bloc.reshape(4,4))
+print(bloc.reshape(n,n))
 
 #correct_bloc = bloc
 locations_w_ones = [i for i, bit in enumerate(bloc) if bit]
